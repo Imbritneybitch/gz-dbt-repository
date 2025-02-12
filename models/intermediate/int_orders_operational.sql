5ba4a4b320ced626e0e698c0dfa025d53531f2a7
Julie Pierre <julie.pierreorcet@gmail.com>
2025-02-12 10:33:10 +0000

    int_orders_operational

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,3 +1,16 @@
-SELECT margin,
-FROM {{ ref('int_sales_margin') }}
+ SELECT
+     o.orders_id
+     ,o.date_date
+     ,ROUND(o.sum_margin + s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin
+     ,o.sum_quantity
+     ,o.sum_revenue
+     ,o.sum_purchase_cost
+     ,o.sum_margin
+     ,s.shipping_fee
+     ,s.logcost
+     ,s.ship_cost
+ FROM {{ref("int_orders_margin")}} o
+ LEFT JOIN {{ref("stg_raw__ship")}} s
+     USING(orders_id)
+     ORDER BY orders_id desc
 
